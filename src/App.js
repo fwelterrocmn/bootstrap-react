a4d0bab149eac38ad9e04e37b81ac625e7c4ca9e
fwelterrocmn <567607@edu.rocmn.nl>
2023-12-04 10:46:23 +0100

    week-4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from './Components/Buttons.js';
+import Card from './Components/Card.js';
+import Container from './Components/layout/Container.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Container>
+        <Card
+          title="title"
+          text="blablabla"
+          image="https://the-artifice.com/wp-content/uploads/2023/03/signalis-scene.jpg"
+          actions={
+            <>
+              <Button variant="success" text="Primary" />
+              <Button variant="danger" text="Danger" />
+            </>
+          }
+        />
+      </Container>
     </div>
   );
 }
